b6fd4c5ffbf442cf974cfce73f386d26ca59d1d5
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-08-03 16:34:14 +0530

    sta section

--- components/sections/Navbar.tsx
+++ components/sections/Navbar.tsx
@@ -4,8 +4,11 @@ export default function Navbar() {
   return (
     <nav className="p-4 shadow bg-white flex gap-6">
       <Link href="/">Home</Link>
-      <Link href="/blogs">Blogs</Link>
-      <Link href="/news">News</Link>
+      <Link href="#about">About us</Link>
+      <Link href="#services">Services</Link>
+      <Link href="/blogs">News & Updates</Link>
+      <Link href="/news">Blogs</Link>
+      <Link href="/blogs">Contact Us</Link>
     </nav>
   );
 }
